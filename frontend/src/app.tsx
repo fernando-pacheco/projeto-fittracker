84693d652188b550313a600ab7e54014bf2f209f
Fernando Pacheco <131721500+fernando-pacheco@users.noreply.github.com>
2024-07-16 19:23:33 -0300

    front-[feat] use router-dom to navigate

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,16 +1,24 @@
-import {
-  createBrowserRouter,
-  RouterProvider
-} from "react-router-dom"
-import { MainInfo } from "./pages/main"
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainInfo />
-  },
-])
+import { Routes, Route } from "react-router-dom";
+import { Dashboard } from "./pages/dashboard";
+import { Programs } from "./pages/programs";
+import { Header } from "./components/header";
+import { Groups } from "./pages/groups";
+import { Templates } from "./pages/templates";
+import { Calendar } from "./pages/calendar";
+import { Chats } from "./pages/chats";
 
 export function App() {
-  return <RouterProvider router={router} />
+  return (
+    <div className="flex flex-col flex-1 space-y-5">
+      <Header />
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/programs" element={<Programs />} />
+        <Route path="/groups" element={<Groups />} />
+        <Route path="/templates" element={<Templates />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/chats" element={<Chats />} />
+      </Routes>
+    </div>
+  );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './app'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { App } from './app';
+import './index.css';
+import { SideBar } from './components/sidebar';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <div className='flex h-screen px-10 py-6'>
+        <SideBar />
+        <App />
+      </div>
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
