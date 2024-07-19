366914f4106ebce2d2304faab5d1ea9fb3217db1
Fernando Pacheco <131721500+fernando-pacheco@users.noreply.github.com>
2024-07-19 11:16:36 -0300

    front-[feat] repaginação na home

---
+++ frontend/src/pages/home/index.tsx
@@ -0,0 +1,14 @@
+import { HomeTabs } from "@/components/home-tab/tabs";
+import { CarouselPlugin } from "./carousel";
+
+
+export function Home() {
+    return (
+        <div className="bg-zinc-900 flex h-full">
+            <HomeTabs />
+            <div className="flex w-1/4 items-center justify-end">
+                <CarouselPlugin />
+            </div>
+        </div>
+    );
+}
