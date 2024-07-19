d136bb13ef42dd3490813b4bb925e37950427490
Fernando Pacheco <131721500+fernando-pacheco@users.noreply.github.com>
2024-07-19 11:01:24 -0300

    front-[refact] ajustes nas configurações do projeto

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,22 +1,46 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate";
+
 export default {
+  darkMode: ["class"],
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      fontFamily:{
-        sans: 'K2D',
+      fontFamily: {
+        sans: ['K2D', 'sans-serif'],
       },
       boxShadow: {
-        shape:
-          "0px 8px 8px rgba(0, 0, 0, 0.1), 0px 4px 4px rgba(0, 0, 0, 0.1), 0px 2px 2px rgba(0, 0, 0, 0.1), 0px 0px 0px 1px rgba(0, 0, 0, 0.1), inset 0px 0px 0px 1px rgba(255, 255, 255, 0.03), inset 0px 1px 0px rgba(255, 255, 255, 0.03)",
+        'bottom-inner': 'inset 0 -2px 0 rgba(245, 158, 11, 0.05), inset 0 -4px 0 rgba(245, 158, 11, 0.04), inset 0 -6px 0 rgba(245, 158, 11, 0.03), inset 0 -8px 0 rgba(245, 158, 11, 0.02)',
+        'left-inner': 'inset 2px 0 0 rgba(245, 158, 11, 0.05), inset 4px 0 0 rgba(245, 158, 11, 0.04), inset 6px 0 0 rgba(245, 158, 11, 0.03), inset 8px 0 0 rgba(245, 158, 11, 0.02)',
       },
-      backgroundImage: {
-        pattern: 'url(/bg.png)'
-      }
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [],
+  plugins: [tailwindcssAnimate],
 }
