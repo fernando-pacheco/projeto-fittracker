6652b1143e7555b2bd3ba395df92d00b7c61a1b9
Fernando Pacheco <131721500+fernando-pacheco@users.noreply.github.com>
2024-07-28 23:28:28 -0300

    back-[refact] configuração das rotas

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('management.urls'))
+    path('m/', include('management.urls')),
+    path('p/', include('programs.urls')),
 ]
 
 if settings.DEBUG:
