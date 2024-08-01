b2444fc79510627e7b97b4355b34c67b79844f23
Fernando Pacheco <131721500+fernando-pacheco@users.noreply.github.com>
2024-07-31 23:06:06 -0300

    front-[feat] componentes de CRUD do workout

---
+++ frontend/src/components/components-ui/modal-register.tsx
@@ -0,0 +1,62 @@
+import { Button } from "@/components/ui/button";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import { CirclePlus, Save } from "lucide-react";
+import { FormEvent, ReactNode } from "react";
+
+interface ModalRegisterProps {
+    buttonTitle: string;
+    title: string;
+    description: string;
+    children: ReactNode;
+    onSubmit: (event: FormEvent<HTMLFormElement>) => void;
+    isOpen: boolean;
+    onOpenChange: (open: boolean) => void;
+}
+
+export function ModalRegister({
+    buttonTitle,
+    title,
+    description,
+    children,
+    onSubmit,
+    isOpen,
+    onOpenChange,
+}: ModalRegisterProps) {
+    return (
+        <Dialog open={isOpen} onOpenChange={onOpenChange}>
+            <DialogTrigger asChild>
+                <Button className='flex bg-zinc-800 gap-2 items-center p-2 border rounded-lg hover:bg-amber-600'>
+                    <CirclePlus className='size-5' />
+                    {buttonTitle}
+                </Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[625px] bg-zinc-800 border-amber-600">
+                <DialogHeader>
+                    <DialogTitle>{title}</DialogTitle>
+                    <DialogDescription>
+                        {description}
+                    </DialogDescription>
+                </DialogHeader>
+                <form onSubmit={onSubmit}>
+                    <div className="grid gap-4 py-4">
+                        {children}
+                    </div>
+                    <DialogFooter>
+                        <Button type="submit" className="gap-2 hover:bg-amber-600">
+                            <Save className="size-5" />
+                            Salvar
+                        </Button>
+                    </DialogFooter>
+                </form>
+            </DialogContent>
+        </Dialog>
+    );
+}
---
+++ frontend/src/components/components-ui/modal-update.tsx
@@ -0,0 +1,65 @@
+import { Button } from "@/components/ui/button";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import { Ellipsis, Save, Trash2 } from "lucide-react";
+import { FormEvent, ReactNode } from "react";
+
+interface ModalUpdateProps {
+    title: string
+    description: string
+    children: ReactNode
+    onSubmit: (event: FormEvent<HTMLFormElement>) => void
+    onDelete: (event: React.MouseEvent<HTMLButtonElement>) => void
+    isOpen: boolean
+    onOpenChange: (open: boolean) => void
+}
+
+export function ModalUpdate({
+    title,
+    description,
+    children,
+    onSubmit,
+    isOpen,
+    onDelete,
+    onOpenChange,
+}: ModalUpdateProps) {
+    return (
+        <Dialog open={isOpen} onOpenChange={onOpenChange}>
+            <DialogTrigger asChild>
+                <button className='bg-zinc-800 rounded-md p-1 hover:bg-amber-600'>
+                    <Ellipsis className="size-5"/>
+                </button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[625px] bg-zinc-800 border-amber-600">
+                <DialogHeader>
+                    <DialogTitle>{title}</DialogTitle>
+                    <DialogDescription>
+                        {description}
+                    </DialogDescription>
+                </DialogHeader>
+                <form onSubmit={onSubmit}>
+                    <div className="grid gap-4 py-4">
+                        {children}
+                    </div>
+                    <DialogFooter>
+                        <Button onClick={onDelete} className="gap-2 bg-amber-800 hover:bg-amber-600">
+                            <Trash2 className="size-5" />
+                            Excluir
+                        </Button>
+                        <Button type="submit" className="gap-2 hover:bg-amber-600">
+                            <Save className="size-5" />
+                            Salvar
+                        </Button>
+                    </DialogFooter>
+                </form>
+            </DialogContent>
+        </Dialog>
+    );
+}
