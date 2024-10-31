67578e2d088aaa8f28debb776e32d88d8ae75926
fernando-pacheco <fernandopachecopx@gmail.com>
2024-10-31 20:19:00 -0300

    [back] Configuração para utilizar PostgreSQL

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -91,10 +91,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'db'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
+
 
 
 # Password validation
